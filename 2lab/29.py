2904d48db2ff29e0253522cb61f501ca82a03fa9
ssbtvnx <sabinaashimbekova@gmail.com>
2023-01-28 16:25:36 +0600

    list

---
+++ 2lab/29.py
@@ -0,0 +1,4 @@
+thislist=["apple", "banana", "cherry"]
+tropical=["mango", "pineapple", "papaya"]
+thislist.extend(tropical) #to append elements from another list to the current list
+print(thislist)
