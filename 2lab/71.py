a6c8b87f75837c4d8e009a0581d63f1fcbbfa431
ssbtvnx <sabinaashimbekova@gmail.com>
2023-02-03 10:42:50 +0600

    update tuples

---
+++ 2lab/71.py
@@ -0,0 +1,5 @@
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+#convert the tuple into a list, remove "apple", and convert it back into a tuple
