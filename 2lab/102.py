2927620eb42b18806e6bcb1e53835fa4079417d6
ssbtvnx <sabinaashimbekova@gmail.com>
2023-02-03 11:23:02 +0600

    join sets

---
+++ 2lab/102.py
@@ -0,0 +1,7 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.symmetric_difference(y)
+
+print(z)
+#return a set that contains all items from both sets, except items that are present in both
