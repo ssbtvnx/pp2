78bb2e37b09f857c47a4e9b2a4d54ebda3863532
ssbtvnx <sabinaashimbekova@gmail.com>
2023-02-04 16:31:26 +0600

    inheritance

---
+++ 3lab/30.py
@@ -0,0 +1,15 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+x = Student("Mike", "Olsen")
+x.printname()
+# a super() function that will make the child class inherit all the methods and properties from its parent
