d709f75fa956a8dedba063168a18ca96701cf742
ssbtvnx <sabinaashimbekova@gmail.com>
2023-03-10 15:51:56 +0600

    6lab

--- 6lab_builtin/4.py
+++ 6lab_builtin/4.py
@@ -1,8 +1,7 @@
-import math
 import time
 
 x=int(input())
 y=int(input())
 time.sleep(y/1000)
 
-print('Square root of',x, 'after', y, 'miliseconds is', math.sqrt(x))
+print('Square root of',x, 'after', y, 'miliseconds is', x*0.5)
